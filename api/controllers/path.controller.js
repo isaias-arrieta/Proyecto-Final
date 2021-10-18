5e09f305f0a283a9524d5e377b1f1db1d37dcf2f
Ricardo Mar <famar414@gmail.com>
2021-10-17 19:51:15 -0600

    changes in api

--- api/controllers/path.controller.js
+++ api/controllers/path.controller.js
@@ -1,18 +1,22 @@
 function upload(req, res) {
-    if (req.file.filename) {
-        const path ={
-            filetype: req.file.mimeType,
-            name: req,
-            filePath: req.file.filePath,
-            userID: id
-          }
-        models.Paths.create(path).then(result => {
-
+    if (req.files.length >= 0) {
+        req.files.forEach(file => {
+            const id= req.params.id;
+            const path ={
+                filetype: file.mimeType,
+                name: file.filename,
+                filePath: file.filePath,
+                userID: id
+              }
+            models.Paths.create(path).then(result => {
+                res.status(200).json({
+                    message: "Carga existosa",
+                    url: req.filename
+                });
+            });
+            
         });
-        res.status(200).json({
-            message: "Carga existosa",
-            url: req.filename
-        });
+       
     } else {
         res.status(500).json({
             message: "Algo salio mal",

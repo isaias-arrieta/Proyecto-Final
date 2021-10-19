928e8e661cddfb82f1c397ac8702061cd0d8cadd
ricardomcupido <89751998+ricardomcupido@users.noreply.github.com>
2021-10-18 20:40:53 -0600

    Revert "Merge branch 'Ricardo' into master"
    This reverts commit 6ccb8c61d633e03b2a0af6c7c73dc062fcfccd65, reversing
    changes made to 4b91d43d7b603c9088f1b26e3e5fe17455ec4199.

--- api/controllers/path.controller.js
+++ api/controllers/path.controller.js
@@ -1,24 +1,21 @@
 function upload(req, res) {
-
-    if (req.files.length >= 0) {
-        req.files.forEach(file => {
-            const id= req.params.id;
-            const path ={
-                filetype: file.mimeType,
-                name: file.filename,
-                filePath: file.filePath,
-                userID: id
-              }
-            models.Paths.create(path).then(result => {
-                res.status(200).json({
-                    message: "Carga existosa",
-                    url: req.filename
-                });
+    if (req.file.filename) {
+        const path ={
+            filetype: req.file.mimeType,
+            name: req.file.filename,
+            filePath: req.file.filePath,
+            userID: req.file.userID
+          }
+          models.Paths.create(pqth).then(result =>{
+            res.status(201).json({
+                message: "User created successfully",
             });
-            
-        });
-       
-
+        }).catch(error => {
+            res.status(500).json({
+                message: "Error creating user 1",
+                error: error
+            })
+        });      
     } else {
         res.status(500).json({
             message: "Algo salio mal",
--- api/server.js
+++ api/server.js
@@ -1,11 +1,7 @@
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const app = require('./app');
 const port = 3000;
 
-https.createServer({
-    key: fs.readFileSync('my_cert.key'),
-    cert: fs.readFileSync('my_cert.crt')
-  }, app).listen(port, function(){
-    console.log("My HTTPS server listening on port " + port + "...");
-  });
+const server = http.createServer(app);
+
+server.listen(port);
